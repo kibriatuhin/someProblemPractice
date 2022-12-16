e8fb559b1b135330a9f11d5b18b01b49a56207b3
kibriatuhin <tuinn78@gmail.com>
2022-12-16 07:47:39 +0600

    regular practice

--- geeksforGeeks/UpperCaseConversion.java
+++ geeksforGeeks/UpperCaseConversion.java
@@ -2,7 +2,27 @@ package geeksforGeeks;
 
 public class UpperCaseConversion {
 
-    public static void main(String[] args) {
+    public static String transform(String s)
+    {
+        // code here
+        String[] array = s.split(" ");
+        String finalResult = "";
 
+        for (int i=0 ; i<array.length ; i++){
+            String rslt = "";
+            char c = array[i].charAt(0);
+            rslt +=c;
+            String result = rslt.toUpperCase();
+            result+=array[i].substring(1);
+            finalResult+=result;
+            finalResult+=" ";
+        }
+        //System.out.println(finalResult);
+        return finalResult;
+    }
+    public static void main(String[] args) {
+        String s = "i Love tuhin";
+
+        transform(s);
     }
 }
